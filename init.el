555cc8685d495228dd8e24e834d485abafe7c39a
Anton Todorov <atodorov@ermpower.com.au>
2018-12-14 10:24:55 +1100

    Working emacs27.xx

---
+++ init.el
@@ -0,0 +1,26 @@
+(require 'package)
+
+;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("org-plus-contrib" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+;;(package-initialize)
+(require 'use-package)
+(use-package org
+  :ensure org-plus-contrib
+)
+(require 'evil)
+(evil-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-plus-contrib evil-surround evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
